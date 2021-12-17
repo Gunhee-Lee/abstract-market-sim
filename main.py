5bf87b22384999a1e9ae86013afc024b25876f66
Gunhee LEE <gunheecs.lee@samsung.com>
2021-12-17 19:14:53 +0900

    Minimal implementation of class Turn.
    Very simple (later to be expanded via using datetime?).

--- main.py
+++ main.py
@@ -3,7 +3,37 @@
 @author: Gunhee LEE (gunheecs.lee@samsung.com)
 @date: initially created on 2021/12/17.
 """
+from decimal import *
 import yaml
+
+class Market:
+    pass
+
+class Actor:
+    pass
+
+class Turn:
+    def __init__(self, startTurn, endTurn):
+        self.startTurn = startTurn
+        self.endTurn = endTurn
+        self.currentTurn = startTurn
+        self.turnIncrements = 1
+    def proceedTurn(self):
+        print("-- Current turn", self.currentTurn, "starts. --")
+        if self.currentTurn < self.endTurn:
+            self.currentTurn += self.turnIncrements
+            return True
+        else:
+            print("  Reached end of turn", self.endTurn, ".")
+            return False
+
+class Bank:
+    pass
+
+class Treasury:
+    pass
+    
+
 
 def loadConfigYaml():
     print("Loading default config.yaml file...")
